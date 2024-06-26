d62fe301817012947cafc5e37eca400597b1e6dd
Merrick Shurtliff <d00463676@CMP-SCC112-1103.local>
2024-06-26 10:52:56 -0600

    got pets working

--- index.js
+++ index.js
@@ -6,39 +6,42 @@ app.use(express.urlencoded({ extended: true }));
 const cors = require("cors");
 app.use(cors());
 
-app.get("/cats", async (request, response) => {
+app.get("/pets", async (request, response) => {
   // get data from mongoDB
   try {
-    let cats = await model.Cats.find();
-    response.json(cats);
-    console.log(cats);
+    let pets = await model.Pets.find();
+    response.json(pets);
+    console.log(pets);
   } catch {
     response.status(400).send("generic error");
   }
 });
 
-app.post("/cats", async function (request, response) {
+app.post("/pets", async function (request, response) {
   const data = request.body;
 
   try {
-    let newCat = new model.Cats({
+    let newPet = new model.Pets({
       name: data.name,
-      count: data.count,
+      species: data.species,
+      breed: data.breed,
+      age: data.age,
+      gender: data.gender,
     });
-    let error = newCat.validateSync();
+    let error = newPet.validateSync();
     if (error) {
       response.status(400).send(error);
       return;
     }
-    await newCat.save();
-    response.status(201).json(newCat);
+    await newPet.save();
+    response.status(201).json(newPet);
   } catch (error) {
-    console.error("Error fetching cats:", error);
+    console.error("Error fetching pets:", error);
     response.status(400).send(error);
   }
 });
 
-app.delete("/cats/:id", async (request, response) => {
+app.delete("/pets/:id", async (request, response) => {
   try {
     let isDeleted = await model.Cats.findOneAndDelete({
       _id: request.params.id,
